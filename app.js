eb52dfde0a353da21d5c81876520c71f3d0a7bd1
Jeremiah <jeremiahogunleye604@gmail.com>
2023-02-22 06:09:37 +0100

    use lodash, add contents to post, home, compose ejs files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,10 +16,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+let posts = [];
 
 app.get('/', (req, res) => {
-  res.render('home', {homeStartingContent: homeStartingContent});
+  res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+
+  // console.log(posts);
 });
 
 app.get('/about', (req, res) => {
@@ -33,9 +36,25 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
+app.get('/post/:postName', (req, res) => {
+  
+  posts.forEach(post => {
+    if (_.lowerCase(post.title) === _.lowerCase(req.params.postName) ) {
+      res.render('post', {post: post});
+      console.log('Match Found');
+    }
+    res.redirect('/');
+  });
+});
+
 app.post('/compose', function(req, res){
-  console.log(req.body.textBox);
-  res.redirect('/compose')
+  const post = {
+    title : req.body.textBox,
+    content : req.body.post
+  };
+
+  posts.push(post);
+  res.redirect('/');
 })
 
 
